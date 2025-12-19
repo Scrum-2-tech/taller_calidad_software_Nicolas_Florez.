97330e7dd102a075b63ae0baaa9188e9acaaacce
Scrum-2-tech <pflorezp2010@gmail.com>
2025-12-18 19:51:19 -0500

    Change include to require_once for db_connection

--- product_crud.php
+++ product_crud.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 // Incluir la conexión y la lógica de seguridad
-include('db_connection.php');
+require_once('db_connection.php');
 
 // Redirigir si el usuario no está logueado
 if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
